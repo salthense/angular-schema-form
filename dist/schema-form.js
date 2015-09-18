02841191807b666c699e7611a0317c6083e31b9a
Alexander Hense <ahense@saltation.de>
2015-09-18 08:05:57 +0200

    Add new directive to allow using "formulas" for fields
    The new directive adds the possibility describe the calculated value of
    a field by using a "formula" (which is an angular expression) much like
    in spreadsheet software.
    
    It is possible to define fields to be watched and which expression
    should be evaluated. This is more flexible than defining multiple
    "onChange"-properties for different fields and is directly related to
    the field on which it is defined.

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -2088,6 +2088,42 @@ angular.module('schemaForm').directive('sfField',
     }
   ]);
 
+/**
+ * Takes the form definition as argument.
+ * Evaluates the expression passed as "formula" and sets it as the new value of
+ * the underlying model property.
+ */
+angular.module('schemaForm').directive('sfFormula', function() {
+  return {
+    require: 'ngModel',
+    restrict: 'AC',
+    scope: false,
+    link: function(scope, element, attrs, ctrl) {
+      var form = scope.$eval(attrs.sfFormula);
+      //"form" is really guaranteed to be here since the decorator directive
+      //waits for it. But best be sure.
+      if (form && form.formula) {
+        if (form.formula.indexOf(';') != -1) {
+          var formulaParts = form.formula.split(';');
+          var variablesToWatch = formulaParts[0].split(',');
+          var formulaExpression = formulaParts[1];
+
+          var watchVariable = function (variable) {
+            scope.$watch(variable, function() {
+              ctrl.$setViewValue(scope.$eval(formulaExpression));
+              ctrl.$render();
+            });
+          }
+
+          for (var i = 0; i < variablesToWatch.length; i++) {
+            watchVariable(variablesToWatch[i]);
+          }
+        }
+      }
+    }
+  };
+});
+
 angular.module('schemaForm').directive('sfMessage',
 ['$injector', 'sfErrorMessage', function($injector, sfErrorMessage) {
 
