dd61476373aa29f3250ba0aaff0351a818b98d38
Alexander Hense <ahense@saltation.de>
2016-04-08 16:02:56 +0200

    Add matrix directive
    This directive enables two-dimensional matrix-like tables.

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -2201,6 +2201,53 @@ angular.module('schemaForm').directive('sfFormula', function() {
   };
 });
 
+angular.module('schemaForm').directive('sfMatrix', ['sfSelect', 'sfPath', 'schemaForm',
+  function(sel, sfPath, schemaForm) {
+    return {
+      scope: false,
+      link: function(scope, element, attrs, sfSchema) {
+        scope.matrixElements = scope.$eval(attrs.sfMatrix);
+        scope.matrixColumns = scope.form.schema.items.properties.column.enum;
+        scope.matrixRows = scope.form.schema.items.properties.row.enum;
+        scope.matrixMap = {};
+
+        var findElement = function(row, column) {
+          var found = null;
+
+          scope.matrixElements.forEach(function(element) {
+            if (element.row == row && element.column == column) {
+              found = element;
+              return;
+            }
+          });
+
+          return found;
+        }
+
+        var newMatrixElements = [];
+        var counter = 0;
+
+        scope.matrixRows.forEach(function(row) {
+          scope.matrixMap[row] = {};
+
+          scope.matrixColumns.forEach(function(column) {
+            scope.matrixMap[row][column] = counter;
+            counter++;
+
+            newMatrixElements.push(findElement(row, column) || {column: column, row: row, selected: false});
+          });
+        });
+
+        scope.matrixElements.splice(0, scope.matrixElements.length);
+
+        newMatrixElements.forEach(function(element) {
+          scope.matrixElements.push(element);
+        });
+      }
+    };
+  }]
+);
+
 angular.module('schemaForm').directive('sfMessage',
 ['$injector', 'sfErrorMessage', function($injector, sfErrorMessage) {
 
