691eaa96121e0c25fa38dd0839162a59e40cda9f
Alexander Hense <ahense@saltation.de>
2016-04-29 13:18:11 +0200

    f|Add attributes and typeahead builders
    Using the attributes builder, it is possible to append any attribute to
    the input-field of a template. This even enables the use of custom
    directives, as the templates are later ().
    
    The typeahead builder enables the use of the uib-typeahead directive
    from angular-bootstrap. A typeahead function must be supplied in the
    form options.

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -298,6 +298,18 @@ angular.module('schemaForm').provider('sfBuilder', ['sfPathProvider', function(s
         var childFrag = args.build(args.form.items, args.path + '.items', state);
         items.appendChild(childFrag);
       }
+    },
+    typeahead: function(args) {
+      if (args.form.typeahead) {
+        args.fieldFrag.querySelector('input').setAttribute('uib-typeahead', 'value for value in options.typeahead(schema, form, $viewValue)');
+      }
+    },
+    attributes: function(args) {
+      if (args.form.attributes) {
+        args.form.attributes.forEach(function(attributeObject) {
+          args.fieldFrag.querySelector('input').setAttribute(attributeObject['attribute'], attributeObject['value']);
+        });
+      }
     }
   };
   this.builders = builders;
