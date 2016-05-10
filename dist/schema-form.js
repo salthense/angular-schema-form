1bde11a9d63104551f53eaebe1fa67e1eb9a9853
Alexander Hense <ahense@saltation.de>
2016-05-10 14:09:32 +0200

    Remove formula directive
    It makes more sense to handle model changes directly instead of writing
    code within the form definition.

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -2177,46 +2177,6 @@ angular.module('schemaForm').directive('sfField',
     }
   ]);
 
-window.formulaFunctions = {};
-
-/**
- * Takes the form definition as argument.
- * Evaluates the expression passed as "formula" and sets it as the new value of
- * the underlying model property.
- */
-angular.module('schemaForm').directive('sfFormula', ['$window', function($window) {
-  return {
-    require: 'ngModel',
-    restrict: 'AC',
-    scope: false,
-    link: function(scope, element, attrs, ctrl) {
-      var form = scope.$eval(attrs.sfFormula);
-      //"form" is really guaranteed to be here since the decorator directive
-      //waits for it. But best be sure.
-      if (form && form.formula) {
-        if (form.formula.indexOf(';') != -1) {
-          var formulaParts = form.formula.split(';');
-          var variablesToWatch = formulaParts[0].split(',');
-          var formulaExpression = formulaParts[1];
-
-          scope.formulaFunctions = $window.formulaFunctions;
-
-          var watchVariable = function (variable) {
-            scope.$watch(variable, function() {
-              ctrl.$setViewValue(scope.$eval(formulaExpression));
-              ctrl.$render();
-            });
-          }
-
-          for (var i = 0; i < variablesToWatch.length; i++) {
-            watchVariable(variablesToWatch[i]);
-          }
-        }
-      }
-    }
-  };
-}]);
-
 angular.module('schemaForm').directive('sfMatrix', ['sfSelect', 'sfPath', 'schemaForm',
   function(sel, sfPath, schemaForm) {
     return {
