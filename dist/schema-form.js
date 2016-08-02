62916bb9995173537adb05690819a2f5eb21d1b0
Arsenij <aschuetzer@saltation.de>
2016-08-02 13:18:53 +0200

    f|#26646|Erstelle die 'exModule' Directive

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -1978,6 +1978,25 @@ angular.module('schemaForm').directive('sfChanged', function() {
   };
 });
 
+angular.module('schemaForm').directive('sfExmodule', ['$rootScope', 'sfSelect', 'sfPath', 'schemaForm',
+  function ($rootScope, sel, sfPath, schemaForm) {
+    return {
+      require: '^sfSchema',
+      scope: false,
+      link: {
+        pre: function (scope, element, attrs, sfSchema) {
+          scope.form = sfSchema.lookup['f' + attrs.sfField];
+        },
+        post: function (scope, element, attrs, sfSchema) {
+          scope.clickEvent = function () {
+            window.moduleFunctions[scope.form.exmoduleOptions.moduleName].click(scope);
+          };
+        }
+      }
+    };
+  }]
+);
+
 angular.module('schemaForm').directive('sfField',
     ['$parse', '$compile', '$http', '$templateCache', '$interpolate', '$q', 'sfErrorMessage',
      'sfPath','sfSelect',
