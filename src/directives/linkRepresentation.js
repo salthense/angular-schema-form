b513b53175cb6f7945e60491a11befa2266cfea5
Arsenij <aschuetzer@saltation.de>
2016-08-29 15:56:55 +0200

    fixup! f|#27442|Erstelle die Linkrepresentation-Direktive

--- src/directives/linkRepresentation.js
+++ src/directives/linkRepresentation.js
@@ -4,18 +4,16 @@ angular.module('schemaForm').directive('sfLinkRepresentation', ['$rootScope', 's
       require: '^sfSchema',
       scope: false,
       link: {
-        pre: function (scope, element, attrs, sfSchema) {
-        },
         post: function (scope, element, attrs, sfSchema) {
-          var recordId = function () {
+          var getRecordId = function () {
             return scope.$eval('model.' + scope.form.representationSettings.watchFor + '.id');
           };
           var init = function () {
             var id = parseInt(scope.$eval('model.' + scope.form.representationSettings.watchFor + '.id'), 10);
             /* Function from parent scope in gertrude */
             scope.depends(scope.form.representationSettings.schema, id).then(function (model) {
-              scope.reprDatas = model.data;
-              if (scope.reprDatas) {
+              scope.representationDatas = model.data;
+              if (scope.representationDatas) {
                 scope.$evalAsync(function () {
                   scope.isSet = true;
                 });
@@ -35,7 +33,7 @@ angular.module('schemaForm').directive('sfLinkRepresentation', ['$rootScope', 's
             }
           });
           /* init if the record already exists */
-          if (recordId() !== undefined) {
+          if (getRecordId() !== undefined) {
             init();
           }
         }
