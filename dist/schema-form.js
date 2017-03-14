899222aa4378277c91d3ea2d192d6829b3becf99
Arsenij Schützer <aschuetzer@saltation.de>
2017-03-14 14:54:24 +0100

    b|#26153|Measurement|Prüfe ob das Feld richtige Werte beim Neusetzen hat
    Wenn die Länge des Array nicht stimmt, werden die fehlende Elemente
    hinzugefügt.

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -2597,8 +2597,19 @@ angular.module('schemaForm').directive('sfMatrix', ['$rootScope', 'sfSelect', 's
 
 angular.module('schemaForm').directive('measurements', ['$compile', function($compile) {
   return {
+    require: '^sfSchema',
+    link: function (scope, element, attrs, sfSchema) {
+      // look for sf index until it will found
+      var timer = function () {
+        if(element.closest('[sf-field]').length === 0) {
+          setTimeout(timer, 50);
+        } else {
+          scope.form = sfSchema.lookup['f' + element.closest('[sf-field]').attr("sf-field")];
+        }
+      }
+      scope.element = element;
+    },
     controller: ['$scope', function($scope) {
-
       $scope.getLabel = function(titleSchema, index) {
         var label = index + 1;
         if (!titleSchema || titleSchema.length < 1) {
@@ -2613,6 +2624,26 @@ angular.module('schemaForm').directive('measurements', ['$compile', function($co
         }
         $('#measurementContainer' + form.key.join('') + ' input')[0].focus();
       };
+
+      $scope.$watch('modelArray', function(newVal, oldVal) {
+        if (angular.isUndefined(newVal)) {
+          $scope.modelArray = $scope.$eval($($scope.element).first().attr('ng-model') + ' = []');
+        }
+
+        while($scope.modelArray.length < $scope.form.elementCount) {
+          $scope.modelArray.push({});
+        }
+        var deleteEmpty = function () {
+          for(var i = 0; i < $scope.modelArray.length; i++) {
+            try {
+              if($scope.modelArray[i].messwert === null) {
+                delete $scope.modelArray[i];
+              }
+            } catch(e) {}
+          }
+        }
+        $scope.$evalAsync(deleteEmpty);
+      });
     }]
   };
 }]);
