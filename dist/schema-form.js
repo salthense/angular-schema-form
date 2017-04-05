a5b9d1ff774a5251b5bd17672bb815b90bfbebb4
Arsenij Schützer <aschuetzer@saltation.de>
2017-04-05 11:25:25 +0200

    b|N/A|Matrix|Setze die matrix neu nach dem Model Update
    Da die Direktiven für bessere Performance nicht mehr neu aufgebaut
    werden, müssen die das Model watchen und sich automatisch updaten.

--- dist/schema-form.js
+++ dist/schema-form.js
@@ -2478,6 +2478,7 @@ angular.module('schemaForm').directive('sfMatrix', ['$rootScope', 'sfSelect', 's
 
         /* arguments[1] is the scope with model*/
         var unregisterModelUpdate = $rootScope.$on('modelUpdated', function () {
+          updateFn();
           var keys = attrs.sfMatrix.split('[').map(function (key) {
             return key.replace(']', '').split('\'').join('');
           });
@@ -2503,6 +2504,19 @@ angular.module('schemaForm').directive('sfMatrix', ['$rootScope', 'sfSelect', 's
           });
           return finalRow;
         };
+
+        var findElement = function(row, column) {
+          var found = null;
+
+          $scope.matrixElements.forEach(function(element) {
+            if (element.row == row && element.column == column) {
+              found = element;
+              return;
+            }
+          });
+
+          return found;
+        }
 
         var getGroups = function (rows) {
           var counter = -1;
@@ -2555,41 +2569,32 @@ angular.module('schemaForm').directive('sfMatrix', ['$rootScope', 'sfSelect', 's
         $scope.matrixColumns = $scope.form.schema.items.properties.column.enum;
         $scope.matrixRows = $scope.form.schema.items.properties.row.enum;
         $scope.matrixMap = {};
-        $scope.matrixElements = $scope.$eval(attrs.sfMatrix) || [];
 
-        var findElement = function(row, column) {
-          var found = null;
+        var updateFn = function() {
+          $scope.matrixElements = $scope.$eval(attrs.sfMatrix) || [];
 
-          $scope.matrixElements.forEach(function(element) {
-            if (element.row == row && element.column == column) {
-              found = element;
-              return;
-            }
+          var newMatrixElements = [];
+          var counter = 0;
+
+          $scope.matrixRows.forEach(function(row) {
+            $scope.matrixMap[row] = {};
+
+            $scope.matrixColumns.forEach(function(column) {
+              $scope.matrixMap[row][column] = counter;
+              counter++;
+
+              newMatrixElements.push(findElement(row, column) || {column: column, row: row, selected: false});
+            });
           });
 
-          return found;
+          $scope.matrixElements.splice(0, $scope.matrixElements.length);
+
+          newMatrixElements.forEach(function(element) {
+            $scope.matrixElements.push(element);
+          });
+          $scope.$eval(attrs.sfMatrix + '= matrixElements');
         }
-
-        var newMatrixElements = [];
-        var counter = 0;
-
-        $scope.matrixRows.forEach(function(row) {
-          $scope.matrixMap[row] = {};
-
-          $scope.matrixColumns.forEach(function(column) {
-            $scope.matrixMap[row][column] = counter;
-            counter++;
-
-            newMatrixElements.push(findElement(row, column) || {column: column, row: row, selected: false});
-          });
-        });
-
-        $scope.matrixElements.splice(0, $scope.matrixElements.length);
-
-        newMatrixElements.forEach(function(element) {
-          $scope.matrixElements.push(element);
-        });
-        $scope.$eval(attrs.sfMatrix + '= matrixElements');
+        updateFn();
       }
     };
   }]
